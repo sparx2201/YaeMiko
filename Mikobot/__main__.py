5893ca7d1eaadf2615a2b11736890239cf538ab1
rahul torat <lorena203232@proton.me>
2024-03-21 23:47:30 +0530

    Update __main__.py

--- Mikobot/__main__.py
+++ Mikobot/__main__.py
@@ -242,8 +242,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 await IMPORTED["rules"].send_rules(update, args[0], from_pm=True)
 
         else:
-          first_name = update.effective_user.first_name
-            lol = await message.reply_photo(
+             first_name = update.effective_user.first_name
+           await message.reply_photo(
                 photo=str(choice(START_IMG)),
                 reply_markup=InlineKeyboardMarkup(START_BTN),
                 caption=FIRST_PART_TEXT.format(escape_markdown(first_name)),
