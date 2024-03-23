9dac4d6decda4126b2a536123eb3ed26846324f6
rahul torat <lorena203232@proton.me>
2024-03-23 13:06:23 +0530

    Update ban.py

--- Mikobot/plugins/ban.py
+++ Mikobot/plugins/ban.py
@@ -1,6 +1,7 @@
 import html
 
-from telegram import ParseMode, Update
+from telegram.constants import ParseMode
+from telegram import Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
