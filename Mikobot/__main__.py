a7be0b8fae9d62f3ca8972449193fc7a2bb6f84f
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-03-25 15:42:56 +0530

    Update __main__.py

--- Mikobot/__main__.py
+++ Mikobot/__main__.py
@@ -242,21 +242,17 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 await IMPORTED["rules"].send_rules(update, args[0], from_pm=True)
 
         else:
-            first_name = update.effective_user.first_name
-            lol = await message.reply_photo(
-                photo=str(choice(START_IMG)),
-                caption=FIRST_PART_TEXT.format(escape_markdown(first_name)),
-                parse_mode=ParseMode.MARKDOWN,
-            )
             await asyncio.sleep(0.2)
             guu = await update.effective_message.reply_text("⚡️")
             await asyncio.sleep(1.0)
             await guu.delete()  # Await this line
-            await update.effective_message.reply_text(
-                PM_START_TEXT,
+            first_name = update.effective_user.first_name
+            lol = await update.effective_message.reply_photo(
+                photo=str(choice(START_IMG)),
+                caption=FIRST_PART_TEXT.format(escape_markdown(first_name)),
                 reply_markup=InlineKeyboardMarkup(START_BTN),
-                parse_mode=ParseMode.MARKDOWN,
                 disable_web_page_preview=False,
+                parse_mode=ParseMode.MARKDOWN,
             )
             
     else:
