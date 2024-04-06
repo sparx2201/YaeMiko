ddc4906d1e29ca1a3e83f82e598e8df677a022f7
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-04-07 01:26:07 +0530

    Update stickers.py

--- Mikobot/plugins/stickers.py
+++ Mikobot/plugins/stickers.py
@@ -115,14 +115,14 @@ async def kang(update: Update, context: CallbackContext):
         else:
             await msg.reply_text("Yea, I can't kang that.")
 
-   if file_id:
+    if file_id:
         kang_file = await context.bot.get_file(file_id)
         if not is_animated:
             file_path = kang_file.file_path
             file_url = f"https://api.telegram.org/file/bot7045231345:AAE1XuKHhRf2nDAvHSrV00SY0GnATQWGFkM/kang_file.file_path"
-
-            # Download the file using urllib
             urllib.request.urlretrieve(file_url, "kangsticker.png")
+        else:
+            await kang_file.download("kangsticker.tgs")
 
         if args:
             sticker_emoji = str(args[0])
@@ -234,6 +234,7 @@ async def kang(update: Update, context: CallbackContext):
             os.remove("kangsticker.tgs")
     except Exception as e:
         print(e)
+
 
 async def makepack_internal(
     update,
