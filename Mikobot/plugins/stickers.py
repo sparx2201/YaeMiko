c1663ac770e812caea59f8430130c32e5e2b99a0
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-04-06 19:36:34 +0530

    Update stickers.py

--- Mikobot/plugins/stickers.py
+++ Mikobot/plugins/stickers.py
@@ -45,21 +45,25 @@ async def cb_sticker(update: Update, context: CallbackContext):
     msg = update.effective_message
     split = msg.text.split(" ", 1)
     if len(split) == 1:
-      await msg.reply_text("Provide some name to search for pack.")
+        await msg.reply_text("Provide some name to search for pack.")
         return
+
+    combot_stickers_url = "https://combot.org/telegram/stickers?q="
     text = requests.get(combot_stickers_url + split[1]).text
-    soup = bs(text, "lxml")
+    soup = bs(text, "html.parser")
     results = soup.find_all("a", {"class": "sticker-pack__btn"})
     titles = soup.find_all("div", "sticker-pack__title")
+
     if not results:
-     await msg.reply_text("No results found :(.")
+        await msg.reply_text("No results found :(.")
         return
+
     reply = f"Stickers for *{split[1]}*:"
     for result, title in zip(results, titles):
         link = result["href"]
         reply += f"\n• [{title.get_text()}]({link})"
-    msg.reply_text(reply, parse_mode=ParseMode.MARKDOWN)
-
+    
+    await msg.reply_text(reply, parse_mode=ParseMode.MARKDOWN)
 
 async def getsticker(update: Update, context: CallbackContext):
     bot = context.bot
