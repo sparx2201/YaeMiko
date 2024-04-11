27d63997fc56af0fe6e300817d89ca63366083d2
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-04-12 02:27:16 +0530

    Update botadmins.py

--- Mikobot/plugins/botadmins.py
+++ Mikobot/plugins/botadmins.py
@@ -64,7 +64,8 @@ async def botstaff(update: Update, context: ContextTypes.DEFAULT_TYPE):
     reply += "\n\n◊ <b>WOLVES :</b>\n"
     reply += ("\n".join(await get_users_list(context, WOLVES)) or "No additional whitelisted users")
 
-    reply += "\n\n‣ <b> 𝘊𝘩𝘦𝘤𝘬 𝘠𝘰𝘶𝘳 </b> <a href=https://telegra.ph/Disaster-Levels-04-11> 𝘋𝘪𝘴𝘢𝘴𝘵𝘦𝘳 𝘓𝘦𝘷𝘦𝘭</a> \n"
+    reply += '\n\n‣ <b> 𝘊𝘩𝘦𝘤𝘬 𝘠𝘰𝘶𝘳 <a href="https://telegra.ph/Disaster-Levels-04-11"> 𝘋𝘪𝘴𝘢𝘴𝘵𝘦𝘳 𝘓𝘦𝘷𝘦𝘭</a> </b>\n'
+
        
     await update.message.reply_text(reply, parse_mode=ParseMode.HTML)
     LOGGER.info(
