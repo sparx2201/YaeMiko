ad26caa4691c016914d82b14f9c28b2503f336aa
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-04-15 18:57:05 +0530

    Update imagegen.py

--- Mikobot/plugins/imagegen.py
+++ Mikobot/plugins/imagegen.py
@@ -74,7 +74,7 @@ async def generate_image_handler(event, model_id):
                 await reply_message.delete()
 
                 # Send the generated image
-                await event.respond(file=img_url, text="*Created By: @JinX_Ubot*")
+                await event.reply(file=img_url)
             break  # Exit the loop when images are available
         else:
             # Wait for a few seconds before checking again
