0e741cc837a4e1bdc2cc77c306dd8cbcab540435
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-05-10 20:38:15 +0530

    Create post.py

---
+++ Mikobot/utils/post.py
@@ -0,0 +1,12 @@
+
+from aiohttp import ClientSession
+
+session = ClientSession()
+
+async def post(url: str, *args, **kwargs):
+    async with session.post(url, *args, **kwargs) as resp:
+        try:
+            data = await resp.json()
+        except Exception:
+            data = await resp.text()
+    return data
