8a5d1fe00d2d9b55c6114bf55bc88f8bdaebbf9c
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-05-13 22:53:08 +0530

    Update toggle_mongo.py

--- Database/mongodb/toggle_mongo.py
+++ Database/mongodb/toggle_mongo.py
@@ -48,3 +48,22 @@ async def nekomode_on(chat_id: int):
 
 async def nekomode_off(chat_id: int):
     await nekomodedb.insert_one({"chat_id_toggle": chat_id})
+
+
+async def is_nsfw_warn_on(chat_id: int) -> bool:
+    chat = await nsfwdb.find_one({"chat_id": chat_id})
+    return chat
+
+
+async def nsfw_warn_on(chat_id: int):
+    is_nsfw = await is_nsfw_on(chat_id)
+    if is_nsfw:
+        return
+    return await nsfwdb.insert_one({"chat_id": chat_id})
+
+
+async def nsfw_warn_off(chat_id: int):
+    is_nsfw = await is_nsfw_on(chat_id)
+    if not is_nsfw:
+        return
+    return await nsfwdb.delete_one({"chat_id": chat_id})
