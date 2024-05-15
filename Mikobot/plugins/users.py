4e1c2d07528e55f6309163d53c42f29259678b53
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-05-15 23:06:14 +0530

    Update users.py

--- Mikobot/plugins/users.py
+++ Mikobot/plugins/users.py
@@ -141,7 +141,7 @@ async def get_user_id(username: str) -> Union[int, None]:
 
 
 async def broadcast(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    
+    user_id = message.from_user.id
     if user_id not in [OWNER_ID] + DEV_USERS:
         await message.reply_text(
             "You are not authorized to use this command. Only the owner and authorized users can use it."
@@ -213,7 +213,7 @@ async def log_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def chats(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
+    user_id = message.from_user.id
     if user_id not in [OWNER_ID] + DEV_USERS:
         await message.reply_text(
             "You are not authorized to use this command. Only the owner and authorized users can use it."
