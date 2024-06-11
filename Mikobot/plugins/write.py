f1c0f464ee1282a1c1356b964b541b7d59b54c76
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-06-11 22:38:57 +0530

    Create write.py

---
+++ Mikobot/plugins/write.py
@@ -0,0 +1,29 @@
+from pyrogram import filters
+from pyrogram.types import Message
+from Mikobot import app
+import requests
+@app.on_message(filters.command("write"))
+async def handwrite(_, message: Message):
+    if message.reply_to_message:
+        text = message.reply_to_message.text
+    else:
+        text =message.text.split(None, 1)[1]
+    m =await message.reply_text( "`Please wait...,\n\nWriting your text...`")
+    
+    write = requests.get(f"https://apis.xditya.me/write?text={text}").url
+
+    caption = f"""
+**Wʀɪᴛᴛᴇɴ ʙʏ :** [{𝘑𝘪𝘯𝙓}](https://t.me/Jinx_Ubot)
+**Rᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {message.from_user.mention}
+"""
+    await m.delete()
+    await message.reply_photo(photo=write,caption=caption)
+
+__mod_name__ = "WʀɪᴛᴇTᴏᴏʟ"
+
+__help__ = """
+
+ ᴡʀɪᴛᴇs ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ ᴏɴ ᴡʜɪᴛᴇ ᴘᴀɢᴇ ᴡɪᴛʜ ᴀ ᴘᴇɴ 🖊
+
+❍ /write <ᴛᴇxᴛ> *:* ᴡʀɪᴛᴇs ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ.
+ """
